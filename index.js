110a3ebc834628acb6349abbe175e00074dd675f
unknown <maryamihsan601@gmail.com>
2024-02-12 17:15:28 +0300

    objects and array home works

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 var students = [{name : 'maryam' , age:23} , {name:'noor ' , age :13} , {name : 'soundos' , age:25} , {name:'hassan' , age : 30}]
-function showYoungestStudent(students) {
-    if (!students || students.length === 0) {
+function showYoungestStudent(std) {
+    if (!std || std.length === 0) {
         console.log("No students provided or the list is empty.");
         return;
       }
-    
-     
-      var min = students[0].age; 
-      for (var i = 0; i < students.length; i++) {
-        if (students[i].age < min) {
-          min = students[i].age;
+      var min = std[0].age; 
+      for (var i = 0; i < std.length; i++) {
+        if (std[i].age < min) {
+          min = std[i].age;
+          var name = std[i].name
         }
       }
     
-      console.log("The minimum age is " + min);
+      console.log("The minimum age is " , name , min);
     }
     showYoungestStudent(students)
 module.exports = showYoungestStudent;
